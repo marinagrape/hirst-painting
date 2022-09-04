72d380fef377a0c22327dba91684b02cf5d0471a
marina <marinagrape02@gmail.com>
2022-09-04 15:51:43 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,43 @@
+import random
+import colorgram
+import turtle as t
+
+# rgb_colors = []
+# colors = colorgram.extract('spots.jpg', 30)
+# for color in colors:
+#     r = color.rgb.r
+#     g = color.rgb.g
+#     b = color.rgb.b
+#     new_color = (r, g, b)
+#     rgb_colors.append(new_color)
+#
+# print(rgb_colors)
+color_list = [(199, 175, 117), (124, 36, 24), (210, 221, 213), (168, 106, 57), (222, 224, 227), (186, 158, 53), (6, 57, 83), (109, 67, 85), (113, 161, 175), (22, 122, 174), (64, 153, 138), (39, 36, 36), (76, 40, 48), (9, 67, 47), (90, 141, 53), (181, 96, 79), (132, 40, 42), (210, 200, 151), (141, 171, 155), (179, 201, 186), (172, 153, 159), (212, 183, 177), (176, 198, 203), (150, 115, 120), (202, 185, 190), (40, 72, 82), (46, 73, 62), (47, 66, 82)]
+
+tim = t.Turtle()
+t.colormode(255)
+tim.speed("fastest")
+tim.penup()
+tim.hideturtle()
+
+tim.setheading(225)
+tim.forward(300)
+tim.setheading(0)
+num_dots = 100
+
+for d_counts in range (1, num_dots + 1):
+    tim.dot(20, random.choice(color_list))
+    tim.forward(50)
+    if d_counts % 10 == 0:
+        tim.setheading(90)
+        tim.forward(50)
+        tim.setheading(180)
+        tim.forward(500)
+        tim.setheading(0)
+
+
+
+
+
+screen = t.Screen()
+screen.exitonclick()
